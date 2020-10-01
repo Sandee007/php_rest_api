0fcc38801a8ffcc8a684e9ae5eb56fb13f381dd2
Sandee007 <sandeepaalahakoon@gmail.com>
2020-10-01 16:07:32 +0530

    first commit

---
+++ config/Database.php
@@ -0,0 +1,16 @@
+<?php 
+    class Database {
+        //DB params
+        private $host = 'localhost';
+        private $db_name = 'php_rest_api';
+        private $username = 'root';
+        private $password = '';
+        private $conn;
+
+        //db connect
+        public function connect(){
+            $this->conn = null;
+
+            
+        }
+    }
